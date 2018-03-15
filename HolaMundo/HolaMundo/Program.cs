92e55b8cdf6caa3cbadb50d175d61f7f630f1c20
Sergio Hernández <30393456+Sergio100401@users.noreply.github.com>
2018-03-15 23:19:53 +0100

    Add files via upload

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo :D");
+
+            return = 0;
+        }
+    }
+}
